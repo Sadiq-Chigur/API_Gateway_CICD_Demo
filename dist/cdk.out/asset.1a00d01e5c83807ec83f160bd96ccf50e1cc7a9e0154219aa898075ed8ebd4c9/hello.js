583af37776053c8dcef586c3bcfe3112ea1e2415
SadiqGameDev <75724591+SadiqGameDev@users.noreply.github.com>
2025-06-27 18:11:56 +0530

    Updated the scripts.

---
+++ dist/cdk.out/asset.1a00d01e5c83807ec83f160bd96ccf50e1cc7a9e0154219aa898075ed8ebd4c9/hello.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = void 0;
+const handler = async () => {
+    return {
+        statusCode: 200,
+        body: 'Hello from Lambda!',
+    };
+};
+exports.handler = handler;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaGVsbG8uanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi9jZGsub3V0L2Fzc2V0LjFhMDBkMDFlNWM4MzgwN2VjODNmMTYwYmQ5NmNjZjUwZTFjYzdhOWUwMTU0MjE5YWE4OTgwNzVlZDhlYmQ0YzkvaGVsbG8udHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBQU8sTUFBTSxPQUFPLEdBQUcsS0FBSyxJQUFJLEVBQUU7SUFDaEMsT0FBTztRQUNMLFVBQVUsRUFBRSxHQUFHO1FBQ2YsSUFBSSxFQUFFLG9CQUFvQjtLQUMzQixDQUFDO0FBQ0osQ0FBQyxDQUFDO0FBTFcsUUFBQSxPQUFPLFdBS2xCIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNvbnN0IGhhbmRsZXIgPSBhc3luYyAoKSA9PiB7XHJcbiAgcmV0dXJuIHtcclxuICAgIHN0YXR1c0NvZGU6IDIwMCxcclxuICAgIGJvZHk6ICdIZWxsbyBmcm9tIExhbWJkYSEnLFxyXG4gIH07XHJcbn07Il19
---
+++ dist/cdk.out/asset.1a00d01e5c83807ec83f160bd96ccf50e1cc7a9e0154219aa898075ed8ebd4c9/user.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = void 0;
+const handler = async (event) => {
+    return {
+        statusCode: 201,
+        body: 'User created!',
+    };
+};
+exports.handler = handler;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidXNlci5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uL2Nkay5vdXQvYXNzZXQuMWEwMGQwMWU1YzgzODA3ZWM4M2YxNjBiZDk2Y2NmNTBlMWNjN2E5ZTAxNTQyMTlhYTg5ODA3NWVkOGViZDRjOS91c2VyLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFPLE1BQU0sT0FBTyxHQUFHLEtBQUssRUFBRSxLQUFVLEVBQUUsRUFBRTtJQUMxQyxPQUFPO1FBQ0wsVUFBVSxFQUFFLEdBQUc7UUFDZixJQUFJLEVBQUUsZUFBZTtLQUN0QixDQUFDO0FBQ0osQ0FBQyxDQUFDO0FBTFcsUUFBQSxPQUFPLFdBS2xCIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNvbnN0IGhhbmRsZXIgPSBhc3luYyAoZXZlbnQ6IGFueSkgPT4ge1xyXG4gIHJldHVybiB7XHJcbiAgICBzdGF0dXNDb2RlOiAyMDEsXHJcbiAgICBib2R5OiAnVXNlciBjcmVhdGVkIScsXHJcbiAgfTtcclxufTsiXX0=
